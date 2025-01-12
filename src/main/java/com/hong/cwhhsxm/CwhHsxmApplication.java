22a753c312783d8061ab30af67db49f81b566322
cyh <Sevagothon@outlook.com>
2025-01-12 11:37:34 +0800

    the frist try

---
+++ src/main/java/com/hong/cwhhsxm/CwhHsxmApplication.java
@@ -0,0 +1,13 @@
+package com.hong.cwhhsxm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwhHsxmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CwhHsxmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hong/cwhhsxm/CwhHsxmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hong.cwhhsxm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CwhHsxmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
